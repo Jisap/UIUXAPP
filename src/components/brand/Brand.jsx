e0ee74128b529f87e92bedc8f41294efc20dc8ae
Jorge Matos <jisapjisap@gmail.com>
2021-12-04 11:51:20 +0100

    UIUXAPP finalizado

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './brand.css'
+import { google, slack, atlassian, dropbox, shopify } from './imports';
+
+const Brand = () => {
+    return (
+    <div className="gpt3__brand section__padding">
+        <div>
+            <img src={google} alt="google_logo"/>
+        </div>
+        <div>
+            <img src={slack} alt="slack_logo"/>
+        </div>
+        <div>
+            <img src={atlassian} alt="atlassian_logo"/>
+        </div>
+        <div>
+            <img src={dropbox} alt="dropbox_logo"/>
+        </div>
+        <div>
+            <img src={shopify} alt="shopify_logo"/>
+        </div>
+  </div>
+    )
+}
+
+export default Brand
